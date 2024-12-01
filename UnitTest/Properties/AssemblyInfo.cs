35fad4b1eeff44289be0de61cae44316c37cb302
qq959 <you@example.com>
2024-12-01 19:48:20 +0200

    Add project files.

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e45b33c-2eb4-4660-82d8-da3bc636377f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Linq;
+using System.Data.SqlClient;
+using AnalaizerClassLibrary;
+
+namespace YourNamespace.Tests
+{
+    [TestClass]
+    public class AnalyzerTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", "Server = DESKTOP-RGMI6T0;Integrated Security = True;Database = Test", "TestData", DataAccessMethod.Sequential)]
+
+        [TestMethod]
+        public void TestCreateStack()
+        {
+            {
+                {
+                    string expression = (string)TestContext.DataRow["expression"];
+                    string expected = (string)TestContext.DataRow["expected"];
+
+                    // Act
+                    AnalaizerClass.expression = expression;
+                    ArrayList result = AnalaizerClass.CreateStack();
+
+                    // Assert
+                    ArrayList expectedList = new ArrayList(expected.Split(' '));
+                    CollectionAssert.AreEqual(expectedList.ToArray(), result.ToArray(), GetAssertionMessage(expectedList, result));
+                }
+            }
+        }
+
+
+
+
+        private string GetAssertionMessage(ArrayList expected, ArrayList actual)
+        {
+            return $"Expected: [{string.Join(", ", expected.Cast<string>())}], Actual: [{string.Join(", ", actual.Cast<string>())}]";
+        }
+    }
+}
